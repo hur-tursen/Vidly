edd7dc7b388b18a1846eb9366f398873033a9c7a
Hur <hurtursen@hotmail.com>
2021-03-16 17:21:50 +0300

    16.03.2021

--- Vidly/App_Start/BundleConfig.cs
+++ Vidly/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace Vidly
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap3-cerulean.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
         }
     }
 }
